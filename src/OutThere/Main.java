a7410f9e6f2ce6b93d217a0f9036d625e51d0015
Naerd <axelrostagny@hotmail.fr>
2017-02-21 00:30:56 +0100

    Update LostInSpace

---
+++ src/OutThere/Main.java
@@ -0,0 +1,13 @@
+package OutThere;
+
+/**
+ * Created by Axel on 05/12/2016.
+ */
+public class Main {
+
+    public static void main(String[] args) {
+
+        Jeu j = new Jeu();
+        j.menuJeu();
+    }
+}
